ae824f254c34b91324fbae6dc3682710eccf2c43
Muhammad Nurul Afif Maliki <muhammad.maliki@cimbniaga.co.id>
2025-04-15 09:57:57 +0700

    initialize project

---
+++ src/components/layouts/base.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { HTMLAttributes, ReactNode } from "react";
+import { Container } from "../container";
+
+interface LayoutProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+  containerVariant?:
+    | "constrainedPadded"
+    | "narrowConstrainedPadded"
+    | "fullMobileConstrainedPadded"
+    | "breakpointPadded"
+    | "fullMobileBreakpointPadded";
+}
+
+export const Layout = ({
+  children,
+  className: classname,
+  containerVariant = "constrainedPadded",
+  ...props
+}: LayoutProps) => {
+  return (
+    <div {...props}>
+      <main className={`my-20 ${classname || ""}`}>
+        <Container variant={containerVariant}>{children}</Container>
+      </main>
+    </div>
+  );
+};
